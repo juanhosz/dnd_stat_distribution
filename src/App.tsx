9b5a74f6f74d046f299a1a50100b42e105b23254
Juan .H <jhoszowski@fi.uba.ar>
2023-01-19 18:22:07 -0300

    Se Mejora la logica de sumar puntos (se agrega header temporal)

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { StatBox } from './components/stat_box';
 import { Personaje } from './components/types';
+import { Stat_frame } from './components/stat_frame';
 
 function App() {
 
-  let stats = ["Strenght","Dexterity","Constitution","Intelligence","Wisdom","Charisma","Honor"];
-  const [puntos_disponibles, setPuntosDisponibles] = useState(85)
-
-  const[bonusNivel, setbonusNivel] = useState(0)
-
-
-
   return (
-    <div className='centered_parent'>
-      <div>puntos disponibles = {puntos_disponibles}</div>
-      <input placeholder='Nivel del personaje ' onChange={e => setbonusNivel(Number(e.target.value)-1)}></input>
-      <div> bonus de nivel = {bonusNivel}</div>
-      <div className="box_caracteristicas">
-        {stats.map(stat => 
-          <StatBox stat_name = {stat} setPuntosDisponibles = {setPuntosDisponibles} puntos_disponibles = {puntos_disponibles} bonus_nivel = {bonusNivel} setBonusNivel = {setbonusNivel}></StatBox>)}
+    <div>
+      <div className='parent_portrait'>Chelo & Dragons - Distribución de estadísticas</div>
+      <div className='imagen_background'>
+      <Stat_frame></Stat_frame>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/stat_box.tsx
+++ src/components/stat_box.tsx
@@ -1,88 +1,185 @@
 import { useEffect, useState } from "react";
+import { isCallSignatureDeclaration } from "typescript";
 
 export const StatBox = (props: any) =>{
     let stat = props.stat_name;
     let setPuntosDisponibles = props.setPuntosDisponibles;
     let puntos_disponibles = props.puntos_disponibles;
-    let nivel = props.bonus_nivel
-    let setNivel = props.setBonusNivel
+    let ptosnivel = props.bonus_nivel
+    let setPtosNivel = props.setBonusNivel
 
-    const [numero,setNumero] = useState(0)
-    const [estadistica_final,setEstadisticaFinal] = useState(0)
+    const [numero,setNumero] = useState(10)
+    const [estadistica_final,setEstadisticaFinal] = useState(10)
     const [bonus_raza, setBonusRaza] = useState(0)
     const [bonus_nivel, setBonusNivel] = useState(0)
+    const [modificador,setModificador] = useState(0)
 
 
     function setear_caracteristica(stat:any){
-        let numero_final = stat.target.value;
+        let numero_final = Number(stat.target.value);
+        let ptos_disponibles = Number(puntos_disponibles);
+
+
+        if ( numero_final> numero && Math.abs(calcularPuntosUtilizados(numero,numero_final)) > ptos_disponibles){
+            let numero_maximo = calcularNumeroMaximo(numero,numero_final)
+            let ptos_utilizados = calcularPuntosUtilizados(numero,numero_maximo)
+            setPuntosDisponibles(ptos_disponibles + ptos_utilizados);
+            setNumero(numero_maximo);
+            return
+        }
+
         if (numero_final > 13 || (numero == 14)){
-            if (puntos_disponibles < 2){
+            if (ptos_disponibles < 2 && numero_final> numero){
                 console.log("No hay puntos suficientes")
+                return;
             }
-            else{
-                setNumero(numero_final);
-                if (numero_final > numero){
-                    setEstadisticaFinal(Number(estadistica_final)+1)
-                    setPuntosDisponibles(puntos_disponibles-2)
-                }
-                else{
-                    setEstadisticaFinal(Number(estadistica_final)-1)
-                    setPuntosDisponibles(puntos_disponibles+2)
-                }
-            }
+                    //setEstadisticaFinal(Number(estadistica_final)-1)
+                    setPuntosDisponibles(ptos_disponibles+calcularPuntosUtilizados(numero,numero_final))
+                    setNumero(numero_final);
+
             return;
         }
+        if(ptos_disponibles <= 0 && numero_final> numero){
+            console.log("No hay puntos disponbiles")
+            return
+        }
+        //setEstadisticaFinal(Number(estadistica_final)-1)
+        setPuntosDisponibles(ptos_disponibles+calcularPuntosUtilizados(numero,numero_final))
         setNumero(numero_final);
-        if (numero_final > numero){
-            setEstadisticaFinal(Number(estadistica_final)+1)
-            setPuntosDisponibles(puntos_disponibles-1)
-        }
-        else{
-            setEstadisticaFinal(Number(estadistica_final)-1)
-            setPuntosDisponibles(puntos_disponibles+1)
-        }
         return;
 
+    }
+
+
+    function calcularNumeroMaximo(inicial:any, final:any){
+        let sumatoria = 0;
+        let ptos_disponibles = Number(puntos_disponibles)
+            for (let i = inicial; i< final;i++){
+                if (sumatoria == ptos_disponibles){
+                    return i;
+                }
+                if (i >= 13){
+                    sumatoria = sumatoria + 2;
+                }
+                else{
+                    sumatoria++;
+                }
+            }
+        return Number(sumatoria);
+    }
+
+    function calcularPuntosUtilizados(inicial:any,final:any){
+        let sumatoria = 0;
+        if (inicial < final){
+            for (let i = inicial; i< final;i++){
+                if (i >= 13){
+                    sumatoria = sumatoria - 2;
+                }
+                else{
+                    sumatoria--;
+                }
+            }
+        }
+        else{
+            for (let i = final; i< inicial;i++){
+                if (i >= 13){
+                    sumatoria = sumatoria + 2;
+                }
+                else{
+                    sumatoria++;
+                }
+            }
+        }
+        return Number(sumatoria);
+    }
+
+
+
+
+    function calcular_modificador(){
+        let sumatoria = 0;
+        if(estadistica_final === 10){
+
+            setModificador(0);
+            return;
+        }
+        if (estadistica_final > 10){
+            for (let i =0; i < estadistica_final; i++){
+                if (i >= 10){
+                    sumatoria++;
+                }
+            }
+        }
+        else{
+            for (let i = 10; i> estadistica_final; i--){
+                sumatoria--;
+            }
+        }
+        setModificador(Math.floor(sumatoria/2));
     }
 
     useEffect(() =>{
         setEstadisticaFinal(Number(numero) + Number(bonus_nivel) + Number(bonus_raza));
+
+        //calcular_modificador();
     },[bonus_nivel,bonus_raza,numero])
+
+    useEffect(() =>{
+        calcular_modificador();
+    },[estadistica_final])
+
 
 
     function setBonusNivelFuncion(stat:any){
-
-        if (stat < bonus_nivel){
-            setNivel(nivel+1)
-            console.log("se entra aca")
+        let bonus_final = Number(stat);
+        let bonus_inicial = Number(bonus_nivel);
+        let ptos_disponibles = Number(ptosnivel);
+        let sumatoria = 0
+        console.log("Los puntos disponibles son:", ptos_disponibles)
+        if (bonus_final > bonus_inicial){
+            if (ptos_disponibles == 0){
+                setBonusNivel(bonus_inicial);
+            }
+            for (let i = bonus_inicial; i < bonus_final;i++){
+                if (sumatoria == ptos_disponibles){
+                    setBonusNivel(bonus_inicial+sumatoria);
+                    setPtosNivel(ptos_disponibles-sumatoria)
+                    return;
+                }
+                sumatoria++
+            }
+            setBonusNivel(bonus_final);
+            setPtosNivel(ptos_disponibles-sumatoria)
+            return
         }
         else{
-            if (nivel ==0){
-                return
+            for (let i = bonus_inicial; i > bonus_final;i--){
+                sumatoria++
             }
-            setNivel(nivel-1)
+            setBonusNivel(bonus_final);
+            setPtosNivel(ptosnivel+sumatoria)
+            return
         }
-        setBonusNivel(stat)
-        return;
+
     }
 
-    console.log(stat);
+
     return(
         <div className="caracteristica">
             <div className="stat_name">{stat}</div>
-            <input className="caracteristica_input" type="number" placeholder="Estadisticas iniciales" min={0} value = {numero} onChange = {e => setear_caracteristica(e)} onKeyDown={(event) => {
-      event.preventDefault();
-    }}></input>
+            <input className="caracteristica_input" type="number" placeholder="Estadisticas iniciales" min={0} value = {numero.toString()} onChange = {e => setear_caracteristica(e)}
+            onKeyDown = {(event) => {setear_caracteristica(event)}}>
+            </input>
             +
-            <input type="number" placeholder="Bonus de raza" min = {0} onChange = {e => setBonusRaza(Number(e.target.value))} onKeyDown={(event) => {
-      event.preventDefault();
-    }}></input>
+            <input type="number" placeholder="Bonus de raza" min = {0} onChange = {e => setBonusRaza(Number(e.target.value))}>
+            </input>
             +
-            <input type="number" placeholder="Bonus de nivel" min = {0} max = {bonus_nivel+nivel} onChange = {e => setBonusNivelFuncion(Number(e.target.value))} onKeyDown={(event) => {
-      event.preventDefault();
-    }}></input>
+            <input type="number" placeholder="Bonus de nivel" min = {0} max = {bonus_nivel+ptosnivel} title = {"bonus de nivel"} value = {bonus_nivel.toString()} onChange = {e => setBonusNivelFuncion(Number(e.target.value))}> 
+            </input>
+
             <div className="caracteristica_total"> = {estadistica_final}  </div>
             <div></div>
+            <div className="box_modificador">Bonus = {modificador}</div>
         </div>
     );
 }
