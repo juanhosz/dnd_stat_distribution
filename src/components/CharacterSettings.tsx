d3d52d1eca39a18f8b0ba6e71c4eb09f27909f78
juanhosz <jhoszowski@fi.uba.ar>
2023-02-15 17:25:23 -0300

    Resetear tabla => resetear datos

--- src/components/CharacterSettings.tsx
+++ src/components/CharacterSettings.tsx
@@ -101,7 +101,7 @@ export function CharacterSettings(props:any){
       <div> Puntos de nivel disponibles = {settings.BonusNivel}</div>
       <div style={{'color':'red'}}> ¡¡Maximum Ability Score = {settings.AbilityScoreMaximo}!!</div>
     </div>
-    <button className={settingsModule.button_container} onClick= { e => resetElements()}> Resetear Tabla</button>
+    <button className={settingsModule.button_container} onClick= { e => resetElements()}> Resetear Datos</button>
   </div>
   )
 }
